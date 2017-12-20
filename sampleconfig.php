5d1aacea34649d1fbfbf741fd3310ea4d336f703
saurabh <s.c.tripathi@accenture.com>
2017-12-20 12:13:23 +0530

    sample conf

---
+++ sampleconfig.php
@@ -0,0 +1,239 @@
+<?php
+ 
+ 
+
+use someting;
+use someting;
+
+define('someting', 'someting');
+define('someting', 'someting');
+define('someting', 'someting');
+define('someting', 'someting');
+ 
+// target switch, which we should change, target else
+if (isset($_ENV['someenv'])) {
+  switch ($_ENV['someenv']) {
+    case 'prod':
+    $someenv = variable_get('someenv', 'someenv');
+    $someenv = variable_get('someenv', 'someenv');
+    $someenv = variable_get('someenv', 'someenv');
+    $someenv = variable_get('someenv', 'someenv');
+    $someenv = variable_get('someenv', 'someenv');
+    $someenv = variable_get('someenv', 'someenv');
+      break;
+    case 'test':
+    case 'dev':
+    case 'training':
+    case 'dev2':
+    case 'test2':
+    case 'dev3':
+    case 'dev4':
+    case 'test3':
+    case 'test4':
+    case 'dev5':
+    case 'dev6':
+    case 'test5':
+    case 'test6':
+    case 'test7':
+       $someenv = variable_get('someenv', 'someenv');
+    $someenv = variable_get('someenv', 'someenv');
+    $someenv = variable_get('someenv', 'someenv');
+    $someenv = variable_get('someenv', 'someenv');
+    $someenv = variable_get('someenv', 'someenv');
+    $someenv = variable_get('someenv', 'someenv');
+      break;
+    case 'preprod':
+    case 'load':
+    case 'preprod2':    
+       $someenv = variable_get('someenv', 'someenv');
+    $someenv = variable_get('someenv', 'someenv');
+    $someenv = variable_get('someenv', 'someenv');
+    $someenv = variable_get('someenv', 'someenv');
+    $someenv = variable_get('someenv', 'someenv');
+    $someenv = variable_get('someenv', 'someenv');
+      break;
+    default:
+      $someenv = variable_get('someenv', 'someenv');
+    $someenv = variable_get('someenv', 'someenv');
+    $someenv = variable_get('someenv', 'someenv');
+    $someenv = variable_get('someenv', 'someenv');
+    $someenv = variable_get('someenv', 'someenv');
+    $someenv = variable_get('someenv', 'someenv');
+  }
+}
+else { 
+   $someenv = variable_get('someenv', 'someenv');
+    $someenv = variable_get('someenv', 'someenv');
+    $someenv = variable_get('someenv', 'someenv');
+    $someenv = variable_get('someenv', 'someenv');
+    $someenv = variable_get('someenv', 'someenv');
+    $someenv = variable_get('someenv', 'someenv');
+}
+
+define('someting', 'someting');
+define('someting', 'someting');
+define('someting', 'someting');
+define('someting', 'someting');
+
+
+/**
+ * Wrapper class  private $someting;
+ */
+class someting {
+
+  private $someting;
+  private $someting;
+  private $someting;
+
+  /**
+   * Constructor to initiate private $ private $someting; private $someting;;
+   */
+  public function __construct() {
+    $this->someting = someting;
+    $this->someting = someting;
+    $this->someting = someting;
+    $this->someting = someting;
+    $this->someting = someting;
+  }
+
+  /**
+   * someting
+   */
+  public function create_appliction_key($application, $auth_flow = 'ADMIN_NO_SRP_AUTH', $environment = NULL) {
+
+    try {
+
+      $someting = someting;
+
+      if ($someting == 'someting' && $someting == 'someting') {
+        $this->someting = someting;
+        $this->someting = someting;
+        $this->someting = someting;
+        $this->someting = someting;
+        $this->someting = someting;
+        $someting = someting;
+      }
+      elseif ($application == 'someting' && $environment == 'someting') {
+       $this->someting = someting;
+        $this->someting = someting;
+        $this->someting = someting;
+        $this->someting = someting;
+        $this->someting = someting;
+        $someting = someting;
+      }
+
+      /* someting*/
+      $this->someting = new someting([
+        'someting' => 'someting',
+        'someting' => $this->someting,
+        'someting' => [
+                    'someting' => $this->someting,
+                    'someting' => $this->someting,
+                ]
+        ]);
+
+      /* Switch case is to asometing */
+      switch ($application) {
+        case 'someting' : $this->someting = someting;
+                $this->someting = someting;
+                break;
+        case 'someting' : $this->someting = someting;
+                $this->someting = someting;
+                break;
+         case 'someting' : $this->someting = someting;
+                $this->someting = someting;
+                break;
+        default: // Nothing
+      }
+
+      /* someting */
+      $someting = array('someting' => $this->someting, 'someting' => $this->someting);
+
+
+      if ($someting == 'someting' && $someting != NULL) {
+        /* sometingation someting */
+        if ($someting == 'someting') {
+          $someting = array('someting' => $this->someting, 'someting' => $_SESSION['someting'][$someting][$someting]['someting']);
+        }
+      }
+      else {
+        /* someting if someting*/
+        if ($someting == 'someting') {
+          $auth_parameters = array('USERNAME' => $this->app_user_name, 'REFRESH_TOKEN' => $_SESSION['something'][$application]['app_refresh_token']);
+        }
+      }
+      /* someting */
+      $someting = $this->someting->someting([
+                'someting' => $someting,
+                'someting' => $someting,
+               'someting' => $someting,
+                'someting' => $someting,
+
+      /* 'someting' => $someting, */
+      if (!empty($'someting' => $someting,)) {
+        if ($someting->get('someting') != NULL) {
+          $someting = $someting->get('someting');
+          if ($someting == 'MyQuote' && $someting != NULL) {
+            $_SESSION['someting'][$someting][$someting]['someting'] = isset($someting['someting']) ? $someting['someting'] : '';
+             
+          }
+          else {
+            $someting = $someting->get('someting');
+          }
+        }
+      }
+    }
+    catch (Exception $e) {
+      watchdog('someting - ', $e->getMessage());
+    }
+  }
+
+
+  /**
+   * This function is to renew customer JWT
+   */
+  public function someting($someting, $someting) {
+
+    try {
+      /* someting */
+      $this->someting = new someting([
+          'someting' => 'someting',
+          'someting' => $this->someting,
+          'someting' => [
+                    'someting' => $this->someting,
+                    'someting' => $this->someting,
+                ]
+        ]);
+
+      /* someting */
+      if ($someting == 'someting') {
+        $someting = array('someting' => trim($someting), 'someting' => $_SESSION['something']['someting']['someting']);
+      }
+
+      /* API call to get application JWT */
+      $someting = $this->someting->someting([
+                'someting' => $someting,
+                'someting' => $someting,
+            
+                'someting' => $someting]);
+      /* Assiging customer JWT, refresh Token and expire time in session */
+      if (!empty($result)) {
+        if ($result->get('someting') != NULL) {
+          $auth_result = $result->get('someting');
+          $_SESSION['someting']['someting']['someting'] = isset($someting['someting']) ? $auth_result['IdToken'] : '';
+           
+        }
+      }
+    }
+    catch (Exception $e) {
+      watchdog('someting ', $e->someting());
+    }
+  }
+
+ 
+
+ 
+
+ 
+}
+
